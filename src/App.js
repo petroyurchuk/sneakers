e0e266d45b2d025f408e19f96ad2ce42388a623b
PetroYurchuk <petroyurchuk66@gmail.com>
2023-07-08 16:06:52 +0300

    add Drawer component

--- src/App.js
+++ src/App.js
@@ -4,51 +4,6 @@ import Header from "./components/Header";
 function App() {
   return (
     <div className="wrapper clear">
-      <div style={{ display: "none" }} className="overlay">
-        <div className="drawer d-flex flex-column">
-          <h2 className="d-flex justify-between mb-30">
-            Кошик{" "}
-            <img className="cu-p" src="/img/btn-remove.svg" alt="remove" />
-          </h2>
-
-          <div className="items">
-            <div className="cartItem d-flex align-center mb-20">
-              <div
-                style={{
-                  backgroundImage: "url(/img/sneakers/1.jpg)",
-                }}
-                className="cartItemImg "
-              ></div>
-              <div className="mr-20 flex">
-                <p className="mb-5">Чоловічі кросівки Nike Air Max 270</p>
-                <b> 4 599 ₴</b>
-              </div>
-              <img
-                className="remove-btn"
-                src="/img/btn-remove.svg"
-                alt="remove"
-              />
-            </div>
-          </div>
-          <div className="cartTotalBlock">
-            <ul>
-              <li>
-                <span>Total:</span>
-                <div></div>
-                <b>4 499 ₴</b>
-              </li>
-              <li>
-                <span>Tax 5%: </span>
-                <div></div>
-                <b>254.50 ₴</b>
-              </li>
-            </ul>
-            <button className="greenButton">
-              Оформити замовлення <img src="/img/arrow.svg" alt="" />
-            </button>
-          </div>
-        </div>
-      </div>
       <Header />
       <div className="content p-40">
         <div className="d-flex align-center justify-between mb-40">
---
+++ src/components/Drawer.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+const Drawer = () => {
+  return (
+    <div style={{ display: "none" }} className="overlay">
+      <div className="drawer d-flex flex-column">
+        <h2 className="d-flex justify-between mb-30">
+          Кошик <img className="cu-p" src="/img/btn-remove.svg" alt="remove" />
+        </h2>
+
+        <div className="items">
+          <div className="cartItem d-flex align-center mb-20">
+            <div
+              style={{
+                backgroundImage: "url(/img/sneakers/1.jpg)",
+              }}
+              className="cartItemImg "
+            ></div>
+            <div className="mr-20 flex">
+              <p className="mb-5">Чоловічі кросівки Nike Air Max 270</p>
+              <b> 4 599 ₴</b>
+            </div>
+            <img
+              className="remove-btn"
+              src="/img/btn-remove.svg"
+              alt="remove"
+            />
+          </div>
+        </div>
+        <div className="cartTotalBlock">
+          <ul>
+            <li>
+              <span>Total:</span>
+              <div></div>
+              <b>4 499 ₴</b>
+            </li>
+            <li>
+              <span>Tax 5%: </span>
+              <div></div>
+              <b>254.50 ₴</b>
+            </li>
+          </ul>
+          <button className="greenButton">
+            Оформити замовлення <img src="/img/arrow.svg" alt="" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Drawer;
